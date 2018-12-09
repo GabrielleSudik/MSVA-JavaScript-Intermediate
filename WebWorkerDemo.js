1aa1935f50d4a342dd48e494175a84c0f79bd301
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-12-08 23:42:52 -0500

    added messageObject to webworkerdemo

--- WebWorkerDemo.js
+++ WebWorkerDemo.js
@@ -1,11 +1,13 @@
 self.onmessage = function(e){
     //the message is on the data property:
-    var message = e.data;
-    self.postMessage(message + "is processed!");
+    var messageObject = e.data;
+    var message = messageObject.message;
+    self.postMessage(messageObject + "is processed!");
 }
 
 //note to self: you can't get the html page to run from VSCode or VS
 //it IS working if you double-click the html file in the explorer folder
+
 //but the button doesn't work :-/
 
 //this is part of part 3
