36f536850ada786447d26d7b6acde699e346b8c9
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-12-08 22:55:38 -0500

    deleted a bunch of unneeded json files

--- WebWorkerDemo.js
+++ WebWorkerDemo.js
@@ -1,8 +1,12 @@
 self.onmessage = function(e){
     //the message is on the data property:
     var message = e.data;
-    self.postMessage(message + 'is processed!');
+    self.postMessage(message + "is processed!");
 }
 
-//note to self: you can't get the html page to run from VSCode
-//and it's not working from VS. hmm...
+//note to self: you can't get the html page to run from VSCode or VS
+//it IS working if you double-click the html file in the explorer folder
+//but the button doesn't work :-/
+
+//this is part of part 3
+//this is the worker side of the code
--- WebWorkerDemo/WebWorkerDemo.js
+++
@@ -1,7 +0,0 @@
-
-//
-self.onmessage = function(e){
-    //the message is on the data property:
-    var message = e.data;
-    self.postMessage(message + 'is processed!');
-}
